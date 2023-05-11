eaf98caf3b032ae6fcd92c99ef93cdcf69275d70
Mariam Albarghouti <mariamalbarghouti@gmail.com>
2023-05-11 09:53:36 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/optimalsolutions/facerego/face_recognition/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.optimalsolutions.facerego.face_recognition
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
---
+++ lib/api/local_auth_api.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:local_auth_ios/local_auth_ios.dart';
+
+class LocalAuthApi {
+  static final _auth = LocalAuthentication();
+
+  static Future<bool> hasBiometrics() async {
+    try {
+      return await _auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      return false;
+    }
+  }
+
+  static Future<bool> authenticate() async {
+    final isAvailable = await hasBiometrics();
+    if (!isAvailable) return false;
+
+    try {
+      return await _auth.authenticate(
+          options: const AuthenticationOptions(
+            useErrorDialogs: false,
+            stickyAuth: false,
+
+
+          ),
+          // 'Face ID Required
+          localizedReason: 'Scan Face to Authenticate',
+          authMessages: const <AuthMessages>[
+            AndroidAuthMessages(
+              signInTitle: 'Oops! Biometric authentication required!',
+              cancelButton: 'No thanks',
+            ),
+            IOSAuthMessages(
+              cancelButton: 'No thanks',
+            ),
+          ]);
+    } on PlatformException catch (e) {
+      return false;
+    }
+  }
+}
